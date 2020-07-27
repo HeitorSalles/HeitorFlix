3a83026b74742dbad04fd420ab4e46c0370ff069
Heitor Salles <67555032+HeitorSalles@users.noreply.github.com>
2020-07-27 19:02:43 -0300

    Add files via upload

---
+++ src/components/Menu/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Logo from '../../components/assets/img/Logo-HEITORFLIX.png';
+import './Menu.css';
+import ButtonLink from '../components/ButtonLink';
+
+function Menu () {
+    return (
+        <nav className="Menu">
+            <a href="/">
+                <img className="Logo" src={Logo} alt="HeitorFlix logo" />
+            </a>
+
+            <ButtonLink className="ButtonLink" href="/">
+                Novo Vídeo
+            </ButtonLink>
+        </nav>
+    );
+}
+
+export default Menu;
