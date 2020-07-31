ad60115f8c8cde84bfdddf0ec9249d2080d1d8bb
Heitor Salles <heitors.dearaujo@gmail.com>
2020-07-31 17:38:15 -0300

    day5

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
-import CadastroVideo from './pages/cadastro/video'
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import CadastroVideo from './pages/cadastro/video';
 import CadastroCategoria from './pages/cadastro/Categoria';
 
+// Desafio master blaster na descrição
 const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
@@ -18,9 +19,5 @@ ReactDOM.render(
       <Route component={Pagina404} />
     </Switch>
   </BrowserRouter>,
-  
-  //<React.StrictMode>
-    //<App />
-  //</React.StrictMode>,
   document.getElementById('root')
 );
